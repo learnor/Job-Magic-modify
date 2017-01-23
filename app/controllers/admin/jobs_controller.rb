a696ef9c564676a72446672710fa94c1a9ebb50f
Learnor <learnor@gmail.com>
2017-01-23 03:47:34 -0600

    only admin can find "Admin Panel" link

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -49,7 +49,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    if current_user.email != 'a@b.c'
+    if !current_user.admin?
       flash[:alert] = "You are not admin"
       redirect_to :back
     end
