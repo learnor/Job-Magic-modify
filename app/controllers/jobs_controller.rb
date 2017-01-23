781516a40fc0dc2135a3ec69d6e14bd0f8cda107
Learnor <learnor@gmail.com>
2017-01-23 02:54:46 -0600

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @jobs = Job.all
