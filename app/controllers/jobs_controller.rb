dca86564a720f68ab112775aeeab7a9e487d0f6a
Learnor <learnor@gmail.com>
2017-01-23 04:30:30 -0600

    hide "hidden job" in jobs index page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(is_hidden: false)
   end
 
   def show
