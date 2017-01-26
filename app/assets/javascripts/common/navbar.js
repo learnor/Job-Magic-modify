93dc09c2ee45b51fac20e4e2a528be2fbc0d8293
Learnor <learnor@gmail.com>
2017-01-26 02:02:10 -0600

    click logo to go back home page

--- app/assets/javascripts/common/navbar.js
+++ app/assets/javascripts/common/navbar.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 500) {
-            $('#navbar').removeClass("navbar-static-top navbar-trans-black").addClass("navbar-fixed-top navbar-trans-white").fadeIn(500);
+            $('#navbar').removeClass("navbar-static-top").addClass("navbar-fixed-top").fadeIn(500);
         } else if ($(this).scrollTop() < 200) {
-            $('#navbar').removeClass("navbar-fixed-top navbar-trans-white").addClass("navbar-static-top navbar-trans-black").fadeIn(500);
+            $('#navbar').removeClass("navbar-fixed-top").addClass("navbar-static-top").fadeIn(500);
         } else {
             $('#navbar').fadeOut(500);
         }
