b228256891f8a38b8771c94e4129d006522a39ee
Learnor <learnor@gmail.com>
2017-01-26 12:33:16 -0600

    add modal login/registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :jobs
   resources :jobs do
@@ -19,5 +19,4 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
 end
